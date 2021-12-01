9043dd7cbb6cbe63ba3c13ce29900a6c6a171060
Joe Moylan <josephmoylan94@gmail.com>
2021-12-01 20:20:46 +0000

    made an icon and a contact us page

---
+++ contactUs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <?php include_once "subpages/head.html"; ?>
+</head>
+<body>
+<div class="titleBox">
+        <br>
+        <h1>Contact Us</h1>
+        <br>
+    </div>
+    <?php include_once 'subpages/navbar.html'; ?>
+    <br>
+</body>
+</html>
