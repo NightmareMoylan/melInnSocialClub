eb6e6d0daf8bcfda5ceb71345be18308f5cbeab1
Joe Moylan <josephmoylan94@gmail.com>
2021-12-01 17:34:15 +0000

    finished the index page and started a functions page

---
+++ functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mel Inn Functions</title>
+    <link rel="stylesheet"  type = "text/css" href="reset.css">
+    <link rel="stylesheet"  type = "text/css" href="style.css">
+</head>
+<body>
+<div class="titleBox">
+        <br>
+        <h1>Functions</h1>
+        <br>
+    </div>
+    <?php include_once 'subpages/navbar.html'; ?>
+    <br>
+    <div class="mainBody">
+        <br>
+        
+    </div>
+    <?php include_once "subPages/footer.html" ?>
+</body>
+</html>
