0472540fd4b40b9902a04f36584e28e26a36adec
Joe Moylan <josephmoylan94@gmail.com>
2021-12-01 20:10:25 +0000

    finished the members' lounge page

--- membersLounge.php
+++ membersLounge.php
@@ -8,6 +8,25 @@
     <?php include_once "subpages/head.html"; ?>
 </head>
 <body>
-    
+<div class="titleBox">
+        <br>
+        <h1>Members' Lounge</h1>
+        <br>
+    </div>
+    <?php include_once 'subpages/navbar.html'; ?>
+    <br>
+    <div class="mainBodyFunctions">
+        <br>
+        <div class="functionsLeftBox">
+            <p>The Member's lounge is where the locals reside, and is also a nice place for a drink if you're passing by. This is wherte we will often stage live entertainment such as musical performers, kareoke and festive events. There are also darts boards, and a darts team who play out of the club.</p>
+        </div>
+        <div class="functionsPhotos">
+            <img src="images/membersLounge1.jpg">
+            <img src="images/membersLounge2.jpg">
+            <img src="images/dartsTeam.jpg">
+        </div>
+        </div>
+    </div>
+    <?php include_once "subPages/footer.html" ?>
 </body>
 </html>
