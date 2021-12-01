be286b20b6f17fe7026328c7caed258a617c9a0e
Joe Moylan <josephmoylan94@gmail.com>
2021-12-01 18:06:15 +0000

    finished the functions page

--- functions.php
+++ functions.php
@@ -16,9 +16,22 @@
     </div>
     <?php include_once 'subpages/navbar.html'; ?>
     <br>
-    <div class="mainBody">
+    <div class="mainBodyFunctions">
         <br>
-        
+        <div class="functionsLeftBox">
+            <p>We have a large room dedicated to custom parties and functions. If you're planning an event, whether it's a birthday, a wedding, a funeral, a fundraiser or just a party for the sake of a party, we can accomodate you. If you have entertainment in mind, there's a large stage at the heart of the room, and the entertainment you book will be welcome. If you're struggling with booking entertainment, we can also help with that. We can also decorate the place to your liking. for more information about functions, or to get a quote, please <a href="contactUs.php">contact us</a> for more information.</p>
+        </div>
+        <div class="functionsPhotos">
+            <img src="images/functionRoom1.jpg">
+            <img src="images/functionRoom2.jpg">
+            <img src="images/functionRoom3.jpg">
+        </div>
+        <div class="functionsPhotos">
+            <img src="images/functionRoom4.jpg">
+            <img src="images/functionRoom5.jpg">
+            <img src="images/functionRoom6.jpg">
+        </div>
+        </div>
     </div>
     <?php include_once "subPages/footer.html" ?>
 </body>
