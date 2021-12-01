9516b57090d88c4a2a2f732e5f2aa9acef20faf4
Joe Moylan <josephmoylan94@gmail.com>
2021-12-01 11:48:26 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mel Inn Social Club</title>
+    <link rel="stylesheet"  type = "text/css" href="reset.css">
+    <link rel="stylesheet"  type = "text/css" href="style.css">
+</head>
+<body>
+    <div class="titleBox">
+        <br>
+        <h1>Mel Inn Social Club</h1>
+        <br>
+        <h3>Welcome</h3>
+        <br>
+    </div>
+    <?php include_once 'subpages/navbar.html'; ?>
+    <br>
+    <div class="mainBody">
+        <br>   
+        <div class="leftIndex">
+            <h3>Welcome to the Mel Inn Social Club</h3>
+            <br>
+            <p>We're a family run business who serve cold beverages to both members and visitors. We serve Guiness, John Smiths and Mild, as well as a fine selection of lagers such as Birra Moretti and Hop House 13.</p>
+            <br>
+            <p>We often feature live entertainment, which includes live bands and performing artists, as well as showing the football where possible. We also have a darts team who play regularly in the Member's Lounge.</p>
+            <br>
+            <p>We have a large function room with a stage to facilitate all types of parties, as well as a resident caterer and resident performers and DJs who will offer a reasonable price for services. Whether it's a wedding, birthday, funeral or just an excuse to get drunk, we're here every step of the way!</p>
+            <br>
+            <div id="mediaButtonsIndex">
+                <button><a href="https://www.facebook.com/MelInnBootle/?ref=page_internal"><img src="images/facebook.png" id="facebookPic"><p>Find us on Facebook</p></a></button> <p>  </p> <button><a href="https://www.instagram.com/melinnhawthorneroad/"><img src="images/instagram.png" id="instaPic"><p>Find us on Instagram</p></a></button>
+            </div>
+        </div>
+        <div class="rightIndex">
+            <img src="images/outside.jpg" id="outsideImage">
+        </div>
+    </div>
+</body>
+</html>
